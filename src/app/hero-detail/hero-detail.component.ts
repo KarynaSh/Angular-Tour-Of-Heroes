c15a659e232b6f126438cf63b4562f933c1a5294
KarynaSh <98342065+KarynaSh@users.noreply.github.com>
2023-03-30 16:32:48 +0200

    commit

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,16 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit{
 
-  @Input() hero?: Hero;
+  hero: Hero | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private locotion: Location
+  ) { }
 
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.locotion.back();
+  }
 }
+
+
+// Зберігає ActivatedRouteінформацію про маршрут до цього екземпляра HeroDetailComponent. 
+// Цей компонент цікавить параметри маршруту, отримані з URL-адреси. 
+// Параметр "id" - це idгерой, який буде показано.
+
+// Отримує HeroServiceдані про героя з віддаленого сервера, і цей компонент використовує їх, щоб отримати відображення героя.
+
+// locationсервіс Angular для взаємодії з браузером. Ця служба дозволяє повернутися до попереднього перегляду.
+
+// route.snapshot - статичне зображення інформації про маршрут невдовзі після створення компонента.
+// paramMapс - ловник значень параметрів маршруту, отриманих із URL-адреси. Ключ "id"повертає idгероя для отримання.
+// Параметри маршруту завжди є рядками. Функція JavaScript Numberперетворює рядок на число, яким і idповинен бути герой.
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,26 +6,33 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
+
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes'); //отправить сообщение, когда герои будут получены.
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // Наразі припустимо, що герой із вказаним `id` існує завжди.
+    // Обробку помилок буде додано потім.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
   } 
 
 }
 
-//getHeroes - метод для возврата фиктивных героев 
+// getHeroes(), getHero()має асинхронний підпис. Він повертає макет героя як Observable, використовуючи функцію RxJS of(). 
 
 // HeroService.getHeroes() возвращает Observable чтобы он мог использовать HttpClient.get метод Angular 
 // для получения героев и HttpClient.get() возвращал Observable.
 
-//of(HEROES) возвращает объект Observable<Hero[]>, который выдает единственное значение — массив фиктивных героев.
+// of(HEROES) возвращает объект Observable<Hero[]>, который выдает единственное значение — массив фиктивных героев.
 
 
